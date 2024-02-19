ee985062797e7bcdd253ff7c2d652b20c734055d
Brian Bischoff <brian.bischoff@globetracker.com>
2024-02-19 16:21:06 +0000

    fix: Trigger new version for upload

--- src/functions/start.entrypoint.ts
+++ src/functions/start.entrypoint.ts
@@ -3,6 +3,8 @@
 // this is the entrypoint that will be called when the transformer is started
 // -----------------------------------------------------------------------------
 
-export default async function() {
-  console.log("Hello from start");
+export default async function () {
+  console.log(
+    `Initializing push adapter using webhook ${process.env.WEBHOOK_URL}`,
+  );
 }
